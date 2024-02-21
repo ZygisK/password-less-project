9140e23b60ea57835a5f20a4e1cf56295c4954dd
Danielius-Simonavicius <Daniel@theguy.xyz>
2024-02-21 19:00:48 +0000

    added printhello method
    did osmething

---
+++ stuff/main.java
@@ -0,0 +1,13 @@
+package stuff;
+public class main{
+    public static void main(String args[]){
+        printHello();
+    }
+
+    public static void printHello(){
+        System.out.println("hello");
+    }
+
+
+    
+}
