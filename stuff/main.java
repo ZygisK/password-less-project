de0380a225ac2cc9694c57a57c333a42e7bef5cd
Danielius-Simonavicius <Daniel@theguy.xyz>
2024-02-21 19:06:00 +0000

    added somethign else
    did stuff

--- stuff/main.java
+++ stuff/main.java
@@ -2,12 +2,16 @@ package stuff;
 public class main{
     public static void main(String args[]){
         printHello();
+        int result = addNumbers(1,1);
     }
 
     public static void printHello(){
         System.out.println("hello");
     }
 
+    public static int addNumbers(int a, int b){
+        return a+b;
+    }
 
-    
+
 }
