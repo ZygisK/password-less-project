3d34ad398e06660909c03dda469131116db05e24
ZygisK <z.kalenkevicius@yahoo.com>
2024-02-21 21:03:34 +0000

    Update Misha.java

--- MishaTest/Misha.java
+++ MishaTest/Misha.java
@@ -1,7 +1,11 @@
 package MishaTest;
 
 public class Misha {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Misha");
+
+        
+        System.out.println("rela");
     }
 }
