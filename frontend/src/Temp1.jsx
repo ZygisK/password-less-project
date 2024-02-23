d22a9fd0cb42a443f64ef120d3586ec4218b80a1
Skellig325 <deafhole@gmail.com>
2024-02-23 21:24:05 +0000

    FrontEnd React-Vite template
    Created template using vite with react framework

---
+++ frontend/src/Temp1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+function Temp1(){
+    return(
+        <div>
+        <h1 className='temp'>HEllo world</h1>
+    </div>
+    )
+   
+};
+
+export default Temp1
---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Temp1 from './Temp1.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Temp1 />
+  </React.StrictMode>,
+)
